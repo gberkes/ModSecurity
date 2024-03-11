12f2cb2ff440f69bb5a577f044091a5bdd831397
gberkes <gabor@berkes.me>
2024-03-11 15:57:08 +0100

    Restore original: using 'a->m_name;' after 'delete(a);'

--- src/rule_with_actions.cc
+++ src/rule_with_actions.cc
@@ -121,7 +121,9 @@ RuleWithActions::RuleWithActions(
                     m_actionsRuntimePos.push_back(a);
                 }
             } else {
-                delete(a);
+                delete a;
+                std::cout << "General failure, action: " << a->m_name;
+                std::cout << " has an unknown type." << std::endl;
                 throw;
             }
         }
