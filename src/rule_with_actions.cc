d18f5cba9bf270a8d3d9ac213f544545dc052400
gberkes <gabor@berkes.me>
2024-03-11 15:34:24 +0100

    Rollback to using 'delete(a)'

--- src/rule_with_actions.cc
+++ src/rule_with_actions.cc
@@ -121,8 +121,8 @@ RuleWithActions::RuleWithActions(
                     m_actionsRuntimePos.push_back(a);
                 }
             } else {
+                delete(a);
                 assert(false && "The handling of RunTimeBeforeMatchAttemptKind has not been implemented yet.");
-                ((void)0);
             }
         }
         delete actions;
